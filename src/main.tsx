90f4afd3372bab74d6f35aad29b0c9106684e6c6
Viktoriya Datchuk <grelada@tut.by>
2024-08-29 16:54:49 +0300

    feat: create store, add rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.module.css';
+import { setupStore } from './store/store.ts';
+import { Provider } from 'react-redux';
+
+const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
